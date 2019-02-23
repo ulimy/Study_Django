a1a5d64e6b1056041d327c73dad03d8cf65b8293
ulimy <ulim130@naver.com>
2019-02-23 16:13:08 +0900

    feat:create

--- ClassLion/DjangoProject/blog/views.py
+++ ClassLion/DjangoProject/blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
 from .models import Blog
+from django.utils import timezone
 
 # Create your views here.
 def home(request):
@@ -10,3 +11,14 @@ def home(request):
 def detail(request,blog_pk):
     blog_detail = get_object_or_404(Blog,pk=blog_pk)
     return render(request,'blog/detail.html',{'blog' : blog_detail})
+
+def new(request):
+    return render(request,'blog/new.html')
+
+def create(request):
+    blog = Blog()
+    blog.title = request.POST['title']
+    blog.body = request.POST['body']
+    blog.pub_date = timezone.datetime.now()
+    blog.save()
+    return render(request,'blog/detail.html',{'blog' : blog})
