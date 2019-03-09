3ba7de432f2df7b7f748f230db8ebb6f0b65a946
ulimy <ulim130@naver.com>
2019-03-09 20:37:35 +0900

    generic view

--- LikeLion/DjangoProject/snippets/views.py
+++ LikeLion/DjangoProject/snippets/views.py
@@ -1,36 +1,16 @@
-from rest_framework import mixins
 from rest_framework import generics
 from .models import Snippet
 from .serializers import SnippetSerializer
 
 # Create your views here.
-class snippet_List(mixins.ListModelMixin,
-                    mixins.CreateModelMixin,
-                    generics.GenericAPIView):
+class snippet_List(generics.ListCreateAPIView):
     # 이 클래스가 가질 queryset과 serializer 미리 선언
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self,request,*args,**kwargs):
-        # 미리 선언되어 있으므로 보내기만 하면 됨!
-        return self.list(request,*args,**kwargs)
+    # generic이 알아서 보냄 따로 적어줄 필요 X
 
-    def post(self,request,*args,**kwargs):
-        return self.create(request,*args,**kwargs)
-
-class snippet_Detail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.CreateAPIView):
+class snippet_Detail(generics.RetrieveUpdateDestroyAPIView):
 
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-    def get(self,request,*args,**kwargs):
-        return self.retrieve(request,*args,**kwargs)
-
-    def put(self,request,*args,**kwargs):
-        return self.update(request,*args,**kwargs)
-
-    def delete(self,request,*args,**kwargs):
-        return self.destroy(request,*args,**kwargs)
