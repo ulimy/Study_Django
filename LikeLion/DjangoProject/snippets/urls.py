bc346b5b8230452f5093d40c7db9e420742a1d2c
ulimy <ulim130@naver.com>
2019-03-09 20:24:14 +0900

    APIView

--- LikeLion/DjangoProject/snippets/urls.py
+++ LikeLion/DjangoProject/snippets/urls.py
@@ -3,8 +3,8 @@ from snippets import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns =[
-    path('',views.snippet_List),
-    path('detail/<int:pk>',views.snippet_Detail),
+    path('',views.snippet_List.as_view()),
+    path('detail/<int:pk>',views.snippet_Detail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- LikeLion/DjangoProject/snippets/views.py
+++ LikeLion/DjangoProject/snippets/views.py
@@ -1,43 +1,47 @@
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Snippet
 from .serializers import SnippetSerializer
 
 # Create your views here.
-@api_view(['GET','POST'])
-def snippet_List(request,format=None):
+class snippet_List(APIView):
 
-    if request.method == 'GET':
+    def get(self,request,formant=None):
         snippets = Snippet.objects.all()
         serializer = SnippetSerializer(snippets,many=True)
         return Response(serializer.data)
 
-    elif request.method == 'POST':
+    def post(self,request,format=None):
         serializer = SnippetSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data,status=HTTP_201_CREATED)
         return Response(serializer.error,status=HTTP_400_BAD_REQUEST)
 
-@api_view(['GET','PUT','DELETE'])
-def snippet_Detail(request,pk,format=None):
-    try:
-        snippet = Snippet.objects.get(pk=pk)
-    except Snippet.DoesNotExist:
-        return Response(status=HTTP_404_NOT_FOUND)
 
-    if request.method == 'GET':
+class snippet_Detail(APIView):
+
+    # pk로 snippet 가져오는 함수 만들어놓고 가져다 쓰기
+    def get_object(self,pk):
+        try:
+            return Snippet.objects.get(pk=pk)
+        except Snippet.DoesNotExist:
+            return Response(status=HTTP_404_NOT_FOUND)
+
+    def get(self,request,pk,format=None):
+        snippet = self.get_object(pk)
         serializer = SnippetSerializer(snippet)
         return Response(serializer.data)
 
-    elif request.method == 'PUT':
+    def put(self,request,pk,format=None):
+        snippet = self.get_object(pk)
         serializer = SnippetSerializer(snippet,data=request.data)
         if serializer.is_valid():
-            serializer.save
+            serializer.save()
             return Response(serializer.data)
-        return Response(serializer.error,HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+    def delete(self,request,pk,format=None):
+        snippet = self.get_object(pk)
         snippet.delete()
         return Response(status=HTTP_204_NO_CONTENT)
