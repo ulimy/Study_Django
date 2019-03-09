b2170d78a0138bbf523d6039940ca0aa70f7e604
ulimy <ulim130@naver.com>
2019-03-09 15:07:14 +0900

    ready

--- LikeLion/DjangoProject/DjangoProject/settings.py
+++ LikeLion/DjangoProject/DjangoProject/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'polls.apps.PollsConfig',
+    'rest_framework',
+    'snippets.apps.SnippetsConfig',
 ]
 
 MIDDLEWARE = [
