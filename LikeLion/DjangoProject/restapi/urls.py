524e7ad107a48786ab6e1df2efc421b2cb236c67
ulimy <ulim130@naver.com>
2019-04-09 20:13:16 +0900

    apply gitignore

--- LikeLion/DjangoProject/restapi/urls.py
+++
@@ -1,7 +0,0 @@
-from .views import post_list,post_detail
-from django.urls import path
-
-urlpatterns = [
-    path('post/',post_list,name='post-list'),
-    path('post/<int:pk>/',post_detail,name='post-detail')
-]
